ac41205f15d9eabca5f566263ea79fef8b672823
arrynfr <136278316+arrynfr@users.noreply.github.com>
2023-08-19 21:43:12 +0200

    show panic info

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ mod efi;
 use crate::efi::EfiSystemTable;
 
 #[panic_handler]
-fn panic(_panic_message: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    print!("{}", info);
     loop{}
 }
 
 #[no_mangle]
 extern fn efi_main(_handle: u64, table: *mut EfiSystemTable) {
     efi::register_efi_system_table(table);
-    print!("Test\n");
     print!("We're booting in UEFI mode ayyy!\n");
     loop{};
 }
