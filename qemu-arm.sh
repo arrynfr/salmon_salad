f98714a0487dcdc826a314362ba62fe6180c38d7
arrynfr <136278316+arrynfr@users.noreply.github.com>
2023-08-21 04:20:30 +0200

    Add aarch64 qemu script

---
+++ qemu-arm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cargo build
+qemu-system-aarch64 -m 4096M -cpu host -M virt,accel=hvf \
+        -bios OVMF_aarch64.fd -device virtio-gpu-pci \
+        -device virtio-net,netdev=net0 \
+	-netdev user,id=net0,tftp=target/aarch64-unknown-uefi/debug,bootfile=salmon_salad.efi
