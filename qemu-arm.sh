3bb387b47805eae113b47cfd007983c764f4efa1
arrynfr <136278316+arrynfr@users.noreply.github.com>
2023-12-11 18:13:19 +0100

    aarch64: Made aarch64 compile again
    An update for aarch64 was neeed since we haven't
    updated it in a while and it wouldn't compile as is.
    Implemented wait_for_interrupt to enter a low power
    cpu state and updated ``qemu-arm.sh`` to be compatible
    with linux and macOS

--- qemu-arm.sh
+++ qemu-arm.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
+set -e 
+
 cargo build --target aarch64-unknown-uefi
-qemu-system-aarch64 -m 4096M -cpu host -M virt,accel=hvf \
-        -bios OVMF_aarch64.fd -device virtio-gpu-pci \
-        -device virtio-net,netdev=net0 \
+
+
+#if[ "$(uname -m)" == 'aarch64' ]
+#then
+#	case "$(uname -s)" in
+#		Linux*)		accelerator=kvm;;
+#		Darwin*)	accelerator=hvf;;
+#	esac
+#fi
+
+qemu-system-aarch64 \
+	-m 4096M \
+	-cpu cortex-a57 \
+	-M virt \
+    -bios OVMF_aarch64.fd \
+    -device virtio-net,netdev=net0 \
+	-nographic \
 	-netdev user,id=net0,tftp=target/aarch64-unknown-uefi/debug,bootfile=salmon_salad.efi
--- src/arch/aarch64/platform.rs
+++ src/arch/aarch64/platform.rs
@@ -1,1 +1,13 @@
-pub fn moose() {}
+//! Architecture dependent internal low level functions.  
+//! These functions should not be used anywhere outside of 
+//! the x86_64 module as they are strictly processor dependent 
+//! and only make sense in the context of x86_64.
+
+use core::arch::asm;
+
+pub fn wait_for_interrupt() {
+    unsafe {
+        asm!("dsb sy");
+        asm!("wfi");
+    }
+}
