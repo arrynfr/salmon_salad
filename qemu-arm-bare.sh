4b16b82d630b6d82b8755e577e46c93593f867f3
arrynfr <136278316+arrynfr@users.noreply.github.com>
2024-01-14 00:21:51 +0100

    aarch64: write small bare metal hello world

---
+++ qemu-arm-bare.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e 
+
+cargo build --target aarch64-unknown-none-softfloat
+rust-objcopy -O binary target/aarch64-unknown-none-softfloat/debug/salmon_salad target/aarch64-unknown-none-softfloat/debug/salmon_salad.o
+
+qemu-system-aarch64 \
+	-m 4096M \
+	-cpu cortex-a57 \
+	-M virt \
+	-nographic \
+	-serial mon:stdio \
+	-kernel target/aarch64-unknown-none-softfloat/debug/salmon_salad.o
+
--- src/arch/aarch64/boot.s
+++ src/arch/aarch64/boot.s
@@ -1,5 +1,31 @@
 .section .text._start
 .global _start
-_start:
+_start:	
+	adr x1, hello_world
+	bl putstr
+	adr x1, goodbye_world
+	bl putstr
 	wfi
 	b		_start
+
+putchar:
+	mov x0, 0x09000000
+	strb w2, [x0]
+	ret
+
+putstr:
+	mov x8, 0
+	add x8, x30, 0
+.loop:
+	ldrb w2, [x1]
+	bl putchar
+	add x1, x1, 1
+	cmp w2, 0
+	bne .loop
+	mov x30, 0
+	add x30, x8, 0
+	ret
+
+.data
+hello_world:    .asciz "Hello, aarch64 world!\r\n"
+goodbye_world:  .asciz "Goodbye, aarch64 world!\r\n"
