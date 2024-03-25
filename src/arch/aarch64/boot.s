722dbbc710e9f54d4ff5e3f694e54232aa6d67ae
arrynfr <136278316+arrynfr@users.noreply.github.com>
2024-03-25 05:47:14 +0100

    aarch64: add potential smp boot up code (QEMU)
    This code brings up core 1 on the QEMU virt board.
    It's not flexible and will be refactored.
    I was just curious how SMP would be done, but we are
    far from worrying about it.

--- src/arch/aarch64/boot.s
+++ src/arch/aarch64/boot.s
@@ -1,19 +1,48 @@
 .section .text._start
 .global _start
+.equ PSCI_0_2_FN64_CPU_ON, 0xc4000003
 _start:
-	#Enable floating point bits FPEN
-	mrs    x1, cpacr_el1
-	mov    x0, #(3 << 20)
-	orr    x0, x1, x0
-	msr    cpacr_el1, x0
+# With this we can enable additional cores on the QEMU virt board
+#.core_bringup:
+	#mrs x2, MPIDR_EL1
+	#and x2, x2, #0xFF
+	#cmp x2, 0
+	#bne .stack_init
+	#ldr x0, =PSCI_0_2_FN64_CPU_ON
+	#mov x1, 1 // enable core 1
+	#adrp x2, _start // pc start address
+	#mov x3, 0
+	#hvc 0
+	#cmp x0, 0
+	#bne .core_fail
+
+.stack_init:
+	adrp x1, _stack_end
+	add x1, x1, 4
+	mrs x2, MPIDR_EL1
+	and x2, x2, #0xFF
+	ldr x3, =_stack_size
+	mul x3, x2, x3
+	sub x1, x1, x3
+	mov sp, x1
+
+	cmp x2, 0
+	bne core_say_hello
 	
-	adr x1, hello_world
-	bl putstr
-	adrp x0, _stack_end
-	mov sp, x0
+	#Enable floating point bits FPEN
+	mrs x1, cpacr_el1
+	mov x0, #(3 << 20)
+	orr x0, x1, x0
+	msr cpacr_el1, x0
+	isb
+	
 	b _start_rust
-	adr x1, goodbye_world
+
+.core_fail:
+	adr x1, bringup_failed
 	bl putstr
+
+.kend:
 	wfi
 	b		_start
 
@@ -35,6 +64,20 @@ putstr:
 	add x30, x8, 0
 	ret
 
+core_say_hello:
+	adr x1, core_hello
+	bl putstr
+	mrs x2, MPIDR_EL1
+	and x2, x2, #0xFF
+	add x2, x2, 0x30
+	bl putchar
+	adr x1, new_line
+	bl putstr
+	b .kend
+
 .data
 hello_world:    .asciz "Hello, aarch64 world!\r\n"
 goodbye_world:  .asciz "Goodbye, aarch64 world!\r\n"
+bringup_failed: .asciz "Failed to bring up cores!\r\n"
+core_hello: 	.asciz "Booted core "
+new_line:		.asciz "\r\n"
