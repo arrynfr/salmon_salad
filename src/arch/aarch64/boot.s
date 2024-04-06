b9e77455413e369b4f6849a09617319398a51601
arrynfr <136278316+arrynfr@users.noreply.github.com>
2024-04-06 03:52:50 +0200

    aarch64: make executable relocatable
    Now we can put our aarch64 executable
    anywhere in memory and relocate it at runtime
    This breaks using cargo run to run the binary
    for now due to rust-objcopy being required
    afterwards. This can be fixed by writing a
    cargo build script some time in the future

--- src/arch/aarch64/boot.s
+++ src/arch/aarch64/boot.s
@@ -1,24 +1,45 @@
 .section .text._start
 .global _start
 .global _per_core_setup
+.type _start, @function
+.equ R_AARCH64_RELATIVE, 1027
 _start:
 	mrs x0, MPIDR_EL1
 	and x0, x0, #0xFF
 	cmp x0, xzr
 	bne _per_core_setup
 
+	adrp x0, _base
+    mov x20, x0
+    adrp x1, _rela_start
+    add x1, x1, :lo12:_rela_start
+    adrp x2, _rela_end
+    add x2, x2, :lo12:_rela_end
+
+.relocate_binary:
+	ldp x25, x26, [x1], 0x10
+	ldr x27, [x1], 0x8
+	cmp x26, R_AARCH64_RELATIVE
+	bne .end_reloc
+	add x22, x0, x25
+	add x23, x0, x27
+	str x23, [x22]
+	cmp x1, x2
+	bne .relocate_binary
+	
+.end_reloc:
 	mrs x19, CurrentEL
 	lsr x19, x19, #2
 	cmp x19, 1
 	beq _per_core_setup
 
-	adr x0, err_el_not_supported
-	bl putstr
-	add w0, w19, 0x30
-	bl putchar
-	adr x0, new_line
-	bl putstr
-	b .kend
+	#adr x0, err_el_not_supported
+	#bl _putstr
+	#add w0, w19, 0x30
+	#bl _putchar
+	#adr x0, new_line
+	#bl _putstr
+	#b .kend
 
 _per_core_setup:
 	#Enable floating point bits FPEN
@@ -42,6 +63,7 @@ _per_core_setup:
 
 	isb
 	dsb sy
+	
 	bl _start_rust // Core passed as func arg in x0
 
 .kend:
@@ -49,17 +71,21 @@ _per_core_setup:
 	wfi
 	b		.kend
 
-putchar:
+.global _putchar
+.type _putchar, @function
+_putchar:
 	ldr x9, =_serial_base
 	strb w0, [x9]
 	ret
 
-putstr:
+.global _putstr
+.type _putstr, @function
+_putstr:
 	mov x20, x30
 	mov x10, x0
 .loop:
 	ldrb w0, [x10]
-	bl putchar
+	bl _putchar
 	add x10, x10, #1
 	cmp w0, 0
 	bne .loop
