478ad59aa2e44d8d56628ba3578384bca68b38d4
arrynfr <136278316+arrynfr@users.noreply.github.com>
2024-04-06 03:46:13 +0200

    print: make prints thread safe
    Using an AtomicBool as a lock we now wait until
    the current print is finished before continuing
    to print on another thread

--- src/print.rs
+++ src/print.rs
@@ -1,10 +1,33 @@
-use core::fmt::{Result, Write};
+use core::{fmt::{Result, Write}, sync::atomic::{AtomicBool, Ordering}};
+
 #[cfg(not(feature = "uefi"))]
 use crate::arch;
 
 #[cfg(feature = "uefi")]
 use crate::efi;
+
+static PRINT_LOCK: AtomicBool = AtomicBool::new(false);
 pub struct StringWriter;
+
+impl StringWriter {
+    pub fn aquire_lock() {
+        while PRINT_LOCK.compare_exchange(
+            false,
+            true,
+            Ordering::SeqCst,
+            Ordering::SeqCst,
+        ).is_err() {}
+    }
+
+    pub fn release_lock() {
+        while PRINT_LOCK.compare_exchange(
+            true,
+            false,
+            Ordering::SeqCst,
+            Ordering::SeqCst,
+        ).is_err() {}
+    }
+}
 
 impl Write for StringWriter {
     fn write_str(&mut self, string: &str) -> Result {
@@ -19,9 +42,11 @@ impl Write for StringWriter {
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => {
+        <$crate::print::StringWriter>::aquire_lock();
         let _ = <$crate::print::StringWriter as core::fmt::Write>::write_fmt(
             &mut $crate::print::StringWriter,
             format_args!($($arg)*));
+        <$crate::print::StringWriter>::release_lock();
     }
 }
 
@@ -31,7 +56,6 @@ macro_rules! println {
         $crate::print!("\n\r")
     };
     ($($arg:tt)*) => {
-        $crate::print!($($arg)*);
-        $crate::print!("\n\r")
+        $crate::print!("{}{}", format_args!($($arg)*), "\r\n");
     }
 }
