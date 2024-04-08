7488c3f6194deb95913c81e3a016facb54a75538
arrynfr <136278316+arrynfr@users.noreply.github.com>
2024-04-08 02:21:01 +0200

    aarch64: add support for system timer

--- src/arch/aarch64/platform.rs
+++ src/arch/aarch64/platform.rs
@@ -11,3 +11,35 @@ pub fn wait_for_interrupt() {
         asm!("wfi");
     }
 }
+
+/// This is not very accurate since we're doing an integer division
+pub fn get_current_poweron_time_in_s() -> u32 {
+    (get_system_timer()/get_system_timer_frequency()) as u32
+}
+
+pub fn get_current_poweron_time_in_ms() -> u32 {
+    (get_system_timer()/(get_system_timer_frequency()/1000)) as u32
+}
+
+pub fn get_current_poweron_time_in_us() -> u32 {
+    (get_system_timer()/(get_system_timer_frequency()/1000000)) as u32
+}
+
+#[inline(always)]
+pub fn get_system_timer_frequency() -> u64 {
+    let frq:u64;
+    unsafe { asm!("mrs x0, CNTFRQ_EL0", out("x0") frq); }
+    frq
+}
+
+#[inline(always)]
+pub fn get_system_timer() -> u64 {
+    let current_cnt: u64;
+    unsafe { asm!("mrs x0, CNTPCT_EL0", out("x0") current_cnt); }
+    current_cnt
+}
+
+pub fn busy_sleep_s(delay: u32) {
+    let start_time = get_current_poweron_time_in_s();
+    while get_current_poweron_time_in_s() - start_time < delay {}
+}
