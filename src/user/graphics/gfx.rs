704c737ffc08a369d8369fc35561938d641423fb
arrynfr <136278316+arrynfr@users.noreply.github.com>
2024-04-08 02:45:30 +0200

    gfx: add clear_screen for aarch64 target
    This is about 55 times faster than the one implemented in rust.
    The buffer must be 16 byte aligned and stride must be divisible
    by 16/8/4. This is currently checked with an assert.

--- src/user/graphics/gfx.rs
+++ src/user/graphics/gfx.rs
@@ -1,19 +1,18 @@
-use core::mem::transmute;
 use super::font::*;
+use core::arch::asm;
 
 #[repr(C, align(4))]
 #[derive(Copy, Clone, Debug)]
 pub struct Color {pub b: u16, pub g: u16, pub r: u16}
-pub struct Vec2 {x: usize, y: usize}
 
-#[derive(Debug ,PartialEq)]
+#[derive(Debug ,PartialEq, Clone)]
 pub enum PixelFormat {
     BGR8,
     BGRX8,
     APL
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct GraphicsBuffer {
     address: *mut u8,
     pub size: usize,
@@ -26,6 +25,8 @@ pub struct GraphicsBuffer {
 
 impl GraphicsBuffer {
     pub fn new(addr: *mut u8, sz: usize, str: u32, hr: u32, vr: u32, pf: PixelFormat, bpp: usize) -> Self {
+        assert!(addr as usize % 16 == 0, "Graphics buffer has to be 16 byte aligned!");
+        assert!(str%16 == 0 || str%8 == 0 || str%4 == 0, "Graphics buffer stride needs to be 16|8|4 byte aligned! Got {} {}", sz, str);
         GraphicsBuffer {
             address: addr,
             size: sz,
@@ -33,39 +34,55 @@ impl GraphicsBuffer {
             horizontal_resolution: hr,
             vertical_resolution: vr,
             pixel_format: pf,
-            bpp: bpp
+            bpp
         }
     }
 
-    pub unsafe fn clear_screen(&self) {
-        let fba: *mut u128 = self.address as *mut u128;
-        let col_128: u128 = 0 as u128;
+    #[cfg(target_arch = "aarch64")]
+    unsafe fn _clear_screen_arm(&self) {
+        let fba = self.address;
+        let end = self.size+fba as usize;
+        let w: u64 = 0x0;
+        if (end-fba as usize)%16 == 0 {
+            asm!("1:","stp x2, x2, [x0], #16", "cmp x0, x1", "bne 1b", in("x0") fba, in("x1") end, in("x2") w);
+        } else if (end-fba as usize)%8 == 0 {
+            asm!("1:","str x2, [x0], #8", "cmp x0, x1", "bne 1b", in("x0") fba, in("x1") end, in("x2") w);
+        } else if (end-fba as usize)%4 == 0 {
+            asm!("1:","str w2, [x0], #4", "cmp x0, x1", "bne 1b", in("x0") fba, in("x1") end, in("x2") w);
+        }
+    }
+
+    unsafe fn _clear_screen_generic(&self) {
         if self.size%16 == 0 {
+            let fba: *mut u128 = self.address as *mut u128;
             for x in 0..self.size/16 {
-                fba.add(x).write_volatile(col_128);
-            }
-        } else {
-            for x in 0..self.size {
-                (fba as *mut u8).add(x).write_volatile(0);
+                fba.add(x).write_volatile(0);
             }
         }
+    }
+    
+    #[inline(always)]
+    pub unsafe fn clear_screen(&self) {
+        #[cfg(target_arch = "aarch64")]
+        self._clear_screen_arm();
+        #[cfg(not(target_arch = "aarch64"))]
+        self._clear_screen_generic();
     }
 
     pub unsafe fn draw_pixel(&self, x: isize, y: isize, color: u32) {
-        let stride = self.stride;
         match self.pixel_format {
             PixelFormat::BGR8 => {
-                let i_offset = x+(y*stride as isize)/self.bpp as isize;
+                let i_offset = x+(y*self.horizontal_resolution as isize);
                 let offset: usize = i_offset as usize;
                 if i_offset > 0 && offset < self.size {
-                    let fba = self.address as *mut u8;
+                    let fba = self.address;
                     fba.wrapping_add(offset*self.bpp).write_volatile((color & 0xFF) as u8);
                     fba.wrapping_add(offset*self.bpp+1).write_volatile(((color >> 8) & 0xFF) as u8);
                     fba.wrapping_add(offset*self.bpp+2).write_volatile(((color >> 16) & 0xFF) as u8);
                 }
             },
             PixelFormat::BGRX8 => {
-                let i_offset = x+(y*stride as isize);
+                let i_offset = x+(y*self.horizontal_resolution as isize);
                 let offset: usize = i_offset as usize;
                 let fba = self.address as *mut u32;
                 if i_offset > 0 && offset < self.size {
@@ -73,7 +90,7 @@ impl GraphicsBuffer {
                 }
             }
             PixelFormat::APL => {
-                let i_offset = x+(y*stride as isize);
+                let i_offset = x+(y*self.horizontal_resolution as isize);
                 let offset: usize = i_offset as usize;
                 let fba = self.address as *mut u32;
                 if i_offset > 0 && offset < self.size {
@@ -84,15 +101,15 @@ impl GraphicsBuffer {
     }
 
     pub unsafe fn draw_pixel_col(&self, x: isize, y: isize, color: Color) {
-        let col;
-        if self.pixel_format == PixelFormat::APL {
-            col = 0 | ((color.r & 0x3FF) as u32) << 20 | ((color.g & 0x3FF) as u32) << 10 | ((color.b & 0x3FF) as u32) << 0;
+        let col=if self.pixel_format == PixelFormat::APL {
+            ((color.r & 0x3FF) as u32) << 20 | ((color.g & 0x3FF) as u32) << 10 | ((color.b & 0x3FF) as u32)
         } else {
-            col = ((color.r & 0xFF) as u32) << 16 | ((color.g & 0xFF) << 8) as u32 | ((color.b & 0xFF) as u32);
-        }
+            ((color.r & 0xFF) as u32) << 16 | ((color.g & 0xFF) << 8) as u32 | ((color.b & 0xFF) as u32)
+        };
         self.draw_pixel(x, y, col);
     }
 
+    //TODO: finish
     pub unsafe fn draw_line(&self, start_x: isize, start_y: isize, stop_x: isize, stop_y: isize, color: Color) {
         let dx: isize = stop_x - start_x;
         let dy: isize = stop_y - start_y;
@@ -102,10 +119,10 @@ impl GraphicsBuffer {
         for x in start_x..stop_x {
             self.draw_pixel_col(x, y, color);
             if d > 0 {
-                y = y +1;
-                d = d - 2*dx;
+                y += 1;
+                d -= 2*dx;
             }
-            d = d + 2*dy
+            d += 2*dy
         }
     }
 
