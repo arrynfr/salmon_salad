f814e4c99ee19425010f9a82e9220c3c5492eea1
arrynfr <136278316+arrynfr@users.noreply.github.com>
2024-04-08 02:38:25 +0200

    aarch64: we now must initialize serial with serial_init
    Right now we only support setting it once but that will
    probably change in the future. This makes everything a
    little more dynamic.

--- src/arch/aarch64/serial.rs
+++ src/arch/aarch64/serial.rs
@@ -1,4 +1,4 @@
-use core::ptr::write_volatile;
+use core::sync::atomic::AtomicUsize;
 
 const _UARTLCR_H: usize = 0x02C;
 const _UARTCR: usize = 0x30;
@@ -7,31 +7,35 @@ const UARTFR: usize = 0x018;
 
 const _UARTCR_UART_ENABLE: u8 = 1 << 0;
 
-pub unsafe fn serial_init(base_addr: usize) {
-    let _ptr = base_addr as *mut u8;
-    //let uartcr = ptr.add(UARTLCR_H).read_volatile();
-    //println!("Uart register is: {uartcr:x?}");
-    //ptr.add(UARTCR).write_volatile(uartcr & !(UARTCR_UART_ENABLE));
-}
+static SERIAL_BASE:AtomicUsize = AtomicUsize::new(0);
 
+pub fn serial_init(serial_base: *mut u8) {
+    SERIAL_BASE.compare_exchange(0, serial_base as usize,
+            core::sync::atomic::Ordering::SeqCst, core::sync::atomic::Ordering::SeqCst)
+            .expect("Serial base already set!");
+}
 
 pub fn serial_getchar() -> Option<u8> {
-    let ptr = 0x09000000 as *mut u8;
-    unsafe {
-        if ptr.add(UARTFR).read_volatile() & 1 << 4 == 0 {
-            Some(ptr.add(0).read_volatile())
+    let serial_base = SERIAL_BASE.load(core::sync::atomic::Ordering::Relaxed) as *mut u8;
+    if serial_base != 0 as *mut u8 {
+        unsafe {
+            if serial_base.add(UARTFR).read_volatile() & 1 << 4 == 0 {
+                Some(serial_base.add(0).read_volatile())
+            }
+            else {
+                None
+            }
         }
-        else {
-            None
-        }
+    } else {
+        panic!("Serial port not initialized");
     }
 }
-
+    
 pub unsafe fn serial_putchar(c: char) {
-    let serial_base = 0x09000000 as *mut u8;
-    write_volatile(serial_base, c as u8);
+    let serial_base = SERIAL_BASE.load(core::sync::atomic::Ordering::Relaxed) as *mut u8;
+    serial_base.write_volatile(c as u8);
 }
-
+    
 pub fn serial_puts(string: &str) {
     for c in string.chars() {
         unsafe {
