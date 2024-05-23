d61ef15e095ed7f6f927ed38c9a7d41c292101a4
arrynfr <136278316+arrynfr@users.noreply.github.com>
2024-05-23 22:44:15 +0200

    driver: rtl8139 skeleton

---
+++ src/driver/rtl8139.rs
@@ -0,0 +1,21 @@
+use super::pci::PCIDevice;
+
+pub struct RTL8139 {
+    device: *mut PCIDevice,
+    _io_space: *mut u32,
+    memory_space: *mut u32
+}
+
+impl RTL8139 {
+    pub fn new(dev: *mut PCIDevice, memory_space: usize, io_space: usize) -> Self {
+        RTL8139 {
+            device: dev,
+            _io_space: io_space as *mut u32,
+            memory_space: memory_space as *mut u32
+        }
+    }
+
+    pub fn init() {
+        
+    }
+}
