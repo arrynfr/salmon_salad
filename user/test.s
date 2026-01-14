0d1d1159299cc1ee38b90574955b112d26930dd6
arrynfr <136278316+arrynfr@users.noreply.github.com>
2026-01-14 18:02:53 +0100

    user: Update user binary for more tests

--- user/test.s
+++ user/test.s
@@ -1,6 +1,16 @@
+.text
+.align 2
+.global _start
+_start:
 sub sp, sp, #192
 mov x0, sp
 //svc 0x1
 svc 0x1338
+svc 0x1339
+svc 0x1331
+svc 0x1332
+svc 0x1333
+svc 0x1333
+svc 0x1333
 add sp, sp, #192
 b .
